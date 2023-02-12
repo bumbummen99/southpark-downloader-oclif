245a858c0c429befd308471eb34d14afc8829fe0
Patrick <privat@skyraptor.eu>
2023-02-12 16:32:07 +0000

    Try to fix test

--- test/hooks/init/bootstrap.test.ts
+++ test/hooks/init/bootstrap.test.ts
@@ -4,6 +4,6 @@ describe('hooks', () => {
   test
   .stdout()
   .hook('init', {id: 'hello world'})
-  .do(output => expect(output.stdout).to.contain('Initializing IoC...\nLoading database.json...'))
+  .do(output => expect(output.stdout).to.contain.all.members(['Initializing IoC...', 'Loading database.json...']))
   .it('does boot successfully')
 })
