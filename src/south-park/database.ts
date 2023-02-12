78ea7b633ac78980cef4deae65e58e6dcce3028b
Patrick <privat@skyraptor.eu>
2023-02-12 02:14:26 +0000

    Add url to database class

--- src/south-park/database.ts
+++ src/south-park/database.ts
@@ -1,3 +1,8 @@
+const 
+
 export default class Database {
-  //
+  /**
+   * The URL to the database.json file, wille be used to load the current database definition from the repository
+   */
+  public static url = 'https://raw.githubusercontent.com/bumbummen99/southpark-downloader/database/database.json'
 }
